1620338d19913d5e4888fee05f66cba5cf636924
mehmetakifaksoy <77227227+mehmetakifaksoy@users.noreply.github.com>
2021-01-19 12:27:30 +0300

    Add files via upload

---
+++ pandas-series.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+#data
+numbers = [20,30,40,50]
+letters = ['a','b','c','d',50]
+scaler = 5
+#pandas_series = pd.Series()
+# pandas_series = pd.Series(numbers)
+# pandas_series = pd.Series(letters)
+pandas_series = pd.Series(scaler)
+
+print(pandas_series)
+
